19a5bdf78d4811f427ed8e36c815c185832be0bf
PhillipHenry <PhillHenry@gmail.com>
2023-11-30 10:47:13 +0000

    Generates SQL

--- spark/src/main/scala/uk/co/odinconsultants/documentation_utils/SQLUtils.scala
+++ spark/src/main/scala/uk/co/odinconsultants/documentation_utils/SQLUtils.scala
@@ -1,15 +1,17 @@
 package uk.co.odinconsultants.documentation_utils
 
 import java.lang.reflect.Field
-import scala.reflect.ClassTag
+
 
 object SQLUtils {
-  def createDatumTable[T: ClassTag](tableName: String): String = {
-    val fields: String = implicitly[ClassTag[T]].runtimeClass.getDeclaredFields
+  def createDatumTable(tableName: String, clazz: Class[_]): String = {
+    println(clazz)
+    val fields    : Array[Field] = clazz.getDeclaredFields
+    val fieldTypes: String       = fields
       .map { field: Field =>
         s"${field.getName} ${field.getType.getSimpleName}"
       }
-      .mkString(",\n")
-    s"""CREATE TABLE $tableName ($fields)""".stripMargin
+      .mkString(",\n ")
+    s"""CREATE TABLE $tableName ($fieldTypes)""".stripMargin
   }
 }
--- spark/src/test/scala/uk/co/odinconsultants/documentation_utils/SQLUtilsSpec.scala
+++ spark/src/test/scala/uk/co/odinconsultants/documentation_utils/SQLUtilsSpec.scala
@@ -1,14 +1,17 @@
 package uk.co.odinconsultants.documentation_utils
+import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
+
+case class TestCaseClass(x: String, y: Int)
 
 class SQLUtilsSpec extends AnyWordSpec {
 
-  case class TestCaseClass(x: String, y: Int)
 
   s"SQL generated from ${TestCaseClass.getClass.getSimpleName}" should {
     val tableName = "table_name"
     "reflect its structure" in {
-      val sql = SQLUtils.createDatumTable(tableName)
+      val sql = SQLUtils.createDatumTable(tableName, classOf[TestCaseClass])
+      sql.replace("\n", "") shouldEqual (s"CREATE TABLE $tableName (x String, y int)")
     }
   }
 
